8e233c3d4e82410486bbe57d53112a89ca8b56bf
Elaishere <2530889266@qq.com>
2024-03-02 16:40:49 +0800

    JDK1.8 Learning environment

---
+++ src/com/rst/test/HashTest.java
@@ -0,0 +1,12 @@
+package com.rst.test;
+
+import java.util.HashMap;
+
+public class HashTest {
+    public static void main(String[] args) {
+        HashMap<String, Integer> map = new HashMap<>();
+
+        map.put("sss", 1);
+        map.put("aaa", 2);
+    }
+}
