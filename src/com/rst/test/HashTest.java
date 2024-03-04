fed00d3ce057b09cfb55e5dd6b972a263e3ccd83
Elaishere <2530889266@qq.com>
2024-03-04 14:40:50 +0800

    2024/3/4 String

--- src/com/rst/test/HashTest.java
+++ src/com/rst/test/HashTest.java
@@ -1,5 +1,7 @@
 package com.rst.test;
 
+import org.junit.Test;
+
 import java.util.HashMap;
 
 public class HashTest {
@@ -8,5 +10,26 @@ public class HashTest {
 
         map.put("sss", 1);
         map.put("aaa", 2);
+    }
+
+    @Test
+    public void testIntern() {
+        String a = "aaaa";
+        String b = "aaaa";
+        String c = new String("aaaa");
+        String d = a.intern();
+        String e = c.intern();
+        System.out.println(c == b);
+        System.out.println(c == d);
+        System.out.println(a == d);
+        System.out.println(d.equals(c));
+        System.out.println(c == e);
+        System.out.println("b:" + b.hashCode());
+        System.out.println("c:" + c.hashCode());
+
+        String s1 = "ab" + "cd";
+        String s2 = "abc" + "d";
+        System.out.println(s1 == s2);
+
     }
 }
